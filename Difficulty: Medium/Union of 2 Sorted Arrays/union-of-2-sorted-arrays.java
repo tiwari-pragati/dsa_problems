8c3d02382d75cb9205341ae588a9ad77e42b135c
Pragati Tiwari <2k23.mca2312726@gmail.com>
2025-07-14 15:32:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -0,0 +1,58 @@
+// User function Template for Java
+
+// a,b : the arrays
+class Solution {
+    // Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+       ArrayList<Integer> temp = new ArrayList<>();
+       int n = a.length;
+       int m = b.length;
+       
+       int i = 0, j = 0;
+       while(i < n && j < m){
+           
+           //  skip duplicate elements in  a[] 
+           if(i > 0 && a[i-1] == a[i]){
+               i ++;
+               continue;
+           }    
+          // skip duplicate elements in b[] 
+          if(j > 0 && b[j-1] == b[j]){
+              j ++;
+              continue;
+          }
+          //add elements
+          if(a[i] < b[j]){
+              temp.add(a[i]);
+              i ++;
+          }
+          else if(a[i] > b[j]){
+              temp.add(b[j]);
+              j ++;
+          }
+          else {
+              temp.add(a[i]);
+              i ++;
+              j ++;
+          }
+       }
+       //for a[]
+       while(i < n){
+           if(i > 0 && a[i-1] == a[i]){
+               i ++;
+               continue;
+           }
+           temp.add(a[i]);
+           i ++;
+       }
+       while(j < m){
+           if(j > 0 && b[j - 1] == b[j]){
+               j ++;
+               continue;
+           }
+           temp.add(b[j]);
+           j ++;
+       }
+       return temp;
+    }
+}
